68f7b7272ef8fc40c4304f7b53fc51a98e00194c
Victor Perrier <perrier.victor@gmail.com>
2026-02-26 11:08:33 +0100

    examples for chartgpu_serie
    Added support for sine and cosine series in chartgpu_serie.

--- examples/chartgpu_serie.R
+++ examples/chartgpu_serie.R
@@ -17,3 +17,37 @@ data.frame(
     color = "#b22222",
     lineStyle = list(width = 4)
   )
+
+# You can pass data through chartgpu_serie
+chartgpu() |>
+  chartgpu_serie(
+    name = "sin",
+    label = "Sine",
+    color = "#228B22",
+    data = data.frame(
+      x = seq(-3, 7, by = 1/8),
+      sin = sin(seq(-3, 7, by = 1/8))
+    )
+  )
+
+# With 2 series
+chartgpu() |>
+  chartgpu_serie(
+    name = "sin",
+    label = "Sine",
+    color = "#228B22",
+    data = data.frame(
+      x = seq(-3, 7, by = 1/8),
+      sin = sin(seq(-3, 7, by = 1/8))
+    )
+  ) |>
+  chartgpu_serie(
+    name = "cos",
+    label = "Cosine",
+    color = "#b22222",
+    type = "area",
+    data = data.frame(
+      x = seq(-3, 7, by = 1/8),
+      cos = cos(seq(-3, 7, by = 1/8))
+    )
+  )
